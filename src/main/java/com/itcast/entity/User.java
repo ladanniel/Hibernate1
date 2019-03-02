3b55a5416442061c02412875c8de2718bfcbfa7a
danniel <ldanniel@sina.com>
2019-03-02 20:01:08 +0800

    Hibernate资料

---
+++ src/main/java/com/itcast/entity/User.java
@@ -0,0 +1,34 @@
+package com.itcast.entity;
+//JavaBean
+public class User {
+	private int uid;//hibernate、mybatis等都要求实体类有一个唯一的属性
+	private String username;
+	private String password;
+	private String address;
+	public int getUid() {
+		return uid;
+	}
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+
+}
